d892bbe693562049448bad1fb25f83ea50cdfeef
Steven Smethurst <funvill@funvill.com>
2024-08-27 14:34:28 -0700

    Added Example-electronforge

--- @funvill/add/src/index.ts
+++ @funvill/add/src/index.ts
@@ -1,4 +1,7 @@
+// src/index.ts
+// import * as AddLib from "AddLib";
 import * as AddLib from "../build/Release/AddLib";
-export function GetNumber():number {
-  return AddLib.GetNumber();
+
+export function GetNumber(): number {
+    return AddLib.GetNumber();
 }
